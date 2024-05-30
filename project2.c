4c02da8880419310ededcbfca796db5468a04e52
ishit10 <kapuparaishit18@gmail.com>
2024-05-30 17:58:34 +0530

    Done

---
+++ project2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    // Ans 1
+    // float c, f;
+    // printf("Enter temperature in Celsius: ");
+    // scanf("%f", &c);
+    // f = (c * 9 / 5) + 32;
+    // printf("%.2f Celsius is equal to %.2f Fahrenheit\n", c, f);
+
+
+
+    // Ans 2
+
+//    int basesalary,hra,da,ta;
+//     printf("Enter your base salary :");
+//     scanf("%d",&basesalary);
+//     printf("Enter HRA :");
+//     scanf("%d",&hra);
+//     printf("Enter da :");
+//     scanf("%d",&da);
+//     printf("enter ta :");
+//     scanf("%d",&ta);
+//     printf("your basesalary is %d, your hra is %d, your da is %d and your ta ia %d\n",basesalary,hra,da,ta);
+//     int grossSalary=basesalary+(basesalary*hra/100)+(basesalary*da/100)+(basesalary*ta/100);
+//     printf("grossSalary is %d\n",grossSalary);
+
+
+
+// Ans 3
+
+// float a1,a2,a3;
+// printf("Enter your a1:");
+// scanf("%f",&a1);
+// printf("Enter your a1:");
+// scanf("%f",&a2);
+// a3=180.0-(a1+a2);
+// printf("your a3 is: %.2f",a3);
+
+
+
+    return 0;
+}
